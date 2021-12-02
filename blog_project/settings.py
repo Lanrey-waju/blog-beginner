3cf1ca8650156d88701e34e8f7b1353970aa34db
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-12-02 06:57:53 +0100

    heroku config bug fix. update installed apps

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
